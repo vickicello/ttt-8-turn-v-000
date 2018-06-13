153e3f829f3fd7e28a639c7e096b0aa0e73b51ab
Vicki Hamm <vicki.cello@gmail.com>
2018-06-13 16:01:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board(boardelse
+    display_board(board)
+  else
     turn(board)
   end
 end
